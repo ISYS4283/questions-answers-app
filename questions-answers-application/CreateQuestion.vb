ba46997d86263f7e9b6e2e02cfc09a4e7b0fdf56
cxiang <cxiang@uark.edu>
2017-10-31 08:41:48 -0500

    Add query parameter bind

--- questions-answers-application/CreateQuestion.vb
+++ questions-answers-application/CreateQuestion.vb
@@ -1,3 +1,9 @@
 ﻿Public Class CreateQuestion
-
+    Protected db As db = New db
+    Private Sub btnCreateQuetionSubmit_Click(sender As Object, e As EventArgs) Handles btnCreateQuetionSubmit.Click
+        db.sql = "INSERT INTO questions (question) VALUES (@question)"
+        db.bind("@question", tbCreateQuestion.Text)
+        db.execute()
+        Me.Dispose()
+    End Sub
 End Class
